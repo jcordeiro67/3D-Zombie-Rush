a2ffd1b388aa3660a70f52dd418c59534694e2c7
John Cordeiro <john@johncordeiro.com>
2018-06-04 01:23:40 -0400

    Moving Platforms

---
+++ Assets/Scripts/MyObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyObject : MonoBehaviour {
+
+	[SerializeField] float platformSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.right * (platformSpeed * Time.deltaTime));
+	}
+}
